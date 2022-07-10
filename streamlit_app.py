714ee150d035d1bbcd0dad8ee8e7d261009c8856
MellyMel Melnyk <109020141+ksquinto1229@users.noreply.github.com>
2022-07-10 17:14:24 +0800

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner');
-streamlit.title('breakfast menu');
+streamlit.header('breakfast menu');
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale Spinach & Rocket Smoothie');
+streamlit.text('Hard Boiled Free-Range Egg');
